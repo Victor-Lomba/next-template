e055fee46d613ceeae6c6407e16d6772be91dc6d
victor <victot21@hotmail.com>
2022-03-16 21:38:47 -0300

    Template init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+  experimental: {
+    styledComponents: true,
+  },
 }
 
 module.exports = nextConfig
